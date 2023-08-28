628c201954fdd763eb7abc28eccb4d9afaaac63b
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 14:30:47 -0400

    Update myGroovy.groovy
    correct curly {}

--- vars/myGroovy.groovy
+++ vars/myGroovy.groovy
@@ -1,7 +1,5 @@
 def call(size) {
     size = size ?: 100
-    node(default) {
-        env.MY_GROOVY_RESULT = Math.abs(new Random().nextInt() % [size]) + 1
-        sh "echo ${MY_GROOVY_RESULT}"
-    }
+    env.MY_GROOVY_RESULT = sh(returnStdout: true, script: "echo ${size}")
+    sh "echo ${MY_GROOVY_RESULT}"
 }
