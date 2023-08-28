743fef0c7e8cc30b591e9ca7c92ca23f0361aa17
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 15:24:00 -0400

    Update myRandom.groovy

--- vars/myRandom.groovy
+++ vars/myRandom.groovy
@@ -1,6 +1,7 @@
 def call(size) {
     size = size ?: 100
     env.RANDOM_NUMBER = sh(returnStdout: true, script: "shuf -i 0-${size} -n 1")
+    env.MY_RESULT = env.RANDOM_NUMBER
     sh "echo ${RANDOM_NUMBER}"
     resultInt = env.RANDOM_NUMBER.toInteger();
     if (resultInt > 25) {
