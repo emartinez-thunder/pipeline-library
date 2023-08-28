cc41749e16ce1aa07d76d2aec2f286d445e85c1b
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 14:48:08 -0400

    Update myRandom.groovy
    use shuf for random

--- vars/myRandom.groovy
+++ vars/myRandom.groovy
@@ -1,5 +1,5 @@
 def call(size) {
     size = size ?: 100
-    env.RANDOM_NUMBER = sh(returnStdout: true, script: "echo ${size}")
+    env.RANDOM_NUMBER = sh(returnStdout: true, script: "shuf -i 0-${size} -n 1")
     sh "echo ${RANDOM_NUMBER}"
 }
