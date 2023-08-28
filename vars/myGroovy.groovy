f82096e61bd521de5fbc4c4bf68b973a6bc7b1b8
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 13:55:29 -0400

    Update myGroovy.groovy
    use Math.abs and Random

--- vars/myGroovy.groovy
+++ vars/myGroovy.groovy
@@ -1,5 +1,5 @@
 def call(size) {
     size = size ?: 100
-    env.MY_GROOVY_RESULT = sh(returnStdout: true, script: "echo ${[ $RANDOM % 40 + 10 ]}")
+    env.MY_GROOVY_RESULT = Math.abs(new Random().nextInt() % [size]) + 1
     sh "echo ${MY_GROOVY_RESULT}"
 }
