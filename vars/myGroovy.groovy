d51231834fa4613e40e4720c54456af00dedbeeb
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 13:48:28 -0400

    Update myGroovy.groovy
    escape dollar

--- vars/myGroovy.groovy
+++ vars/myGroovy.groovy
@@ -1,5 +1,5 @@
 def call(size) {
     size = size ?: 100
-    env.MY_GROOVY_RESULT = sh(returnStdout: true, script: "$[ $RANDOM % 40 + 10 ]")
+    env.MY_GROOVY_RESULT = sh(returnStdout: true, script: "echo ${[ $RANDOM % 40 + 10 ]}")
     sh "echo ${MY_GROOVY_RESULT}"
 }
