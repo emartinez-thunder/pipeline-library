29f6a2731abc1afba18378d5631f23597787dc51
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 15:09:43 -0400

    Update myRandom.groovy
    add toInteger

--- vars/myRandom.groovy
+++ vars/myRandom.groovy
@@ -2,7 +2,8 @@ def call(size) {
     size = size ?: 100
     env.RANDOM_NUMBER = sh(returnStdout: true, script: "shuf -i 0-${size} -n 1")
     sh "echo ${RANDOM_NUMBER}"
-    if (env.RANDOM_NUMBER > 25) {
+    resultInt = env.RANDOM_NUMBER.toInteger();
+    if (resultInt > 25) {
         env.SUCCESS = true
     }
     else {
