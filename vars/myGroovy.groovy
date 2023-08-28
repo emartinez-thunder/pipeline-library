ac98605051735aa640a971a47a63dbaf35af23b3
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-28 14:00:41 -0400

    Update myGroovy.groovy
    add node default

--- vars/myGroovy.groovy
+++ vars/myGroovy.groovy
@@ -1,5 +1,7 @@
 def call(size) {
     size = size ?: 100
-    env.MY_GROOVY_RESULT = Math.abs(new Random().nextInt() % [size]) + 1
-    sh "echo ${MY_GROOVY_RESULT}"
+    node(default) {
+        env.MY_GROOVY_RESULT = Math.abs(new Random().nextInt() % [size]) + 1
+        sh "echo ${MY_GROOVY_RESULT}"
+    }
 }
