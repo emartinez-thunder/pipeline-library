0b295233b19745c4b94a6113e3f74be9b1f7d09a
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:23:45 -0400

    Update myRandomText.groovy
    install a dictionary

--- vars/myRandomText.groovy
+++ vars/myRandomText.groovy
@@ -1,6 +1,7 @@
 def call(lines) {
     lines = lines.toInteger() ?: 5
     /* env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")*/
+    sh(returnStdout: true, script: "sudo apt install dict")
     env.RANDOM_LINES = sh(returnStdout: true, script: "ls -ltr /usr/share/dict")
     env.MY_RESULT = env.RANDOM_LINES
     sh "echo ${MY_RESULT}"
