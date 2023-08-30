73533093e4a54dbc6b1bc5e41d1ecfc1e820e637
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:53:28 -0400

    Delete myRandomText.groovy
    abandon talktative

--- vars/myRandomText.groovy
+++
@@ -1,7 +0,0 @@
-def call(lines) {
-    lines = lines.toInteger() ?: 5
-    /* env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")*/
-    sh(returnStdout: true, script: "curl -o resultFile https://random-word-api.herokuapp.com/word?number=10")
-    env.MY_RESULT = sh(returnStdout: true, script: "cat resultFile |tr \",\" \"\\n\" |tr -d \"^ \" > resultFile; cat resultFile; rm resultFile")
-    sh "echo ${MY_RESULT}"
-}
