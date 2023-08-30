a7fcb00175817e8dd2f51f4d8e5f403818cf4d4f
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:49:56 -0400

    Update myRandomText.groovy
    last attempt

--- vars/myRandomText.groovy
+++ vars/myRandomText.groovy
@@ -2,6 +2,6 @@ def call(lines) {
     lines = lines.toInteger() ?: 5
     /* env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")*/
     sh(returnStdout: true, script: "curl -o resultFile https://random-word-api.herokuapp.com/word?number=10")
-    env.MY_RESULT = sh(returnStdout: true, script: "cat resultFile |sed 's/\\,/\\n/g' > resultFile; cat resultFile; rm resultFile")
+    env.MY_RESULT = sh(returnStdout: true, script: "cat resultFile |tr \",\" \"\\n\" |tr -d \"^ \" > resultFile; cat resultFile; rm resultFile")
     sh "echo ${MY_RESULT}"
 }
