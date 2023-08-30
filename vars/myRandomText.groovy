50217b91c643cb8b9eb6a4cfe83092f37977fe63
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:13:19 -0400

    Update myRandomText.groovy
    try the words list in linux

--- vars/myRandomText.groovy
+++ vars/myRandomText.groovy
@@ -1,6 +1,6 @@
 def call(lines) {
     lines = lines.toInteger() ?: 5
-    env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/american-english")
+    env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")
     env.MY_RESULT = env.RANDOM_LINES
     sh "echo ${MY_RESULT}"
 }
