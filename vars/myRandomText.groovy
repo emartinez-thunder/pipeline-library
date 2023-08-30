d493453c7b5e01d6bf9506092e38db271dd126c3
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:33:44 -0400

    Update myRandomText.groovy
    try online api

--- vars/myRandomText.groovy
+++ vars/myRandomText.groovy
@@ -1,8 +1,7 @@
 def call(lines) {
     lines = lines.toInteger() ?: 5
     /* env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")*/
-    sh(returnStdout: true, script: "apt install dict")
-    env.RANDOM_LINES = sh(returnStdout: true, script: "ls -ltr /usr/share/dict")
+    sh(returnStdout: true, script: "https://random-word-api.herokuapp.com/word?number=10")
     env.MY_RESULT = env.RANDOM_LINES
     sh "echo ${MY_RESULT}"
 }
