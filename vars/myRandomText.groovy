0b0095ed24ea4e37ec2c148bf70d0ea294a58b3e
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:16:21 -0400

    Update myRandomText.groovy
    dump contents of dict

--- vars/myRandomText.groovy
+++ vars/myRandomText.groovy
@@ -1,6 +1,7 @@
 def call(lines) {
     lines = lines.toInteger() ?: 5
     env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")
+    env.RANDOM_LINES = sh(returnStdout: true, script: "ls -ltr /usr/share/dict")
     env.MY_RESULT = env.RANDOM_LINES
     sh "echo ${MY_RESULT}"
 }
