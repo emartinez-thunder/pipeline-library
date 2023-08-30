96ad9b79b1c8baabc0971d9ae12027c142f65ae5
Elena Martinez <64991417+EMartinez-demo@users.noreply.github.com>
2023-08-30 15:35:38 -0400

    Update myRandomText.groovy
    forget the curl

--- vars/myRandomText.groovy
+++ vars/myRandomText.groovy
@@ -1,7 +1,7 @@
 def call(lines) {
     lines = lines.toInteger() ?: 5
     /* env.RANDOM_LINES = sh(returnStdout: true, script: "shuf -n${lines} /usr/share/dict/words")*/
-    sh(returnStdout: true, script: "https://random-word-api.herokuapp.com/word?number=10")
+    sh(returnStdout: true, script: "curl https://random-word-api.herokuapp.com/word?number=10")
     env.MY_RESULT = env.RANDOM_LINES
     sh "echo ${MY_RESULT}"
 }
